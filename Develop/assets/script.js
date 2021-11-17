489e258d2b799215cf61444dfac6bf46df4b014e
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-16 19:02:33 -0500

    added flipclock requirment

---
+++ Develop/assets/script.js
@@ -0,0 +1,22 @@
+//version 1
+
+
+//display todays date as a heading
+
+
+//then display a hourly schedule with hour and task and save button as list
+
+
+//in the task there will be an input text box
+
+//if current time is past the hour on the schedule it will be grey
+
+//red is current time hour
+
+//green is future
+
+
+//use localstorage to store the tasks get and setItem
+
+
+//add a flipclock to countdown each hour
