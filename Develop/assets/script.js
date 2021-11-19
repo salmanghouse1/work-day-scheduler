d52dc0a57b62c5d7909ebdec06c1336f6ccf0be3
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-18 20:02:02 -0500

    changed to jquery

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,41 +1,43 @@
 //version 1
+$(document).ready(function() {
+
+    //display todays date as a heading, in todaysDate Id
+
+    var rightNow = new Date();
+
+    console.log(rightNow);
+
+    options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+
+    console.log(rightNow.toLocaleDateString("en-US", options));
 
 
-//display todays date as a heading, in todaysDate Id
+    var rightNowText = JSON.stringify(rightNow.toLocaleDateString("en-US", options))
 
-var rightNow = new Date();
-
-console.log(rightNow);
-
-options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
-
-console.log(rightNow.toLocaleDateString("en-US", options));
+    console.log($("#currentDay"));
 
 
-var rightNowText = JSON.stringify(rightNow.toLocaleDateString("en-US", options))
+    var currentDayID = $("#currentDay");
 
-console.log()
-
-
-var currentDayID = document.getElementById("currentDay");
-
-currentDayID.textContent = rightNowText.split('"')[1];
+    currentDayID.text(rightNowText.split('"')[1]);
 
 
 
-//then display a hourly schedule with hour and task and save button as list
+    //then display a hourly schedule with hour and task and save button as list
 
 
-//in the task there will be an input text box
+    //in the task there will be an input text box
 
-//if current time is past the hour on the schedule it will be grey
+    //if current time is past the hour on the schedule it will be grey
 
-//red is current time hour
+    //red is current time hour
 
-//green is future
-
-
-//use localstorage to store the tasks get and setItem
+    //green is future
 
 
-//add a flipclock to countdown each hour
+    //use localstorage to store the tasks get and setItem
+
+
+    //add a flipclock to countdown each hour
+
+})
