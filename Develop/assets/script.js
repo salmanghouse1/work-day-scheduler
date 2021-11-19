b092313c13d74a9221a9640509e141d99aec8af2
Salman Ghouse <ghousemu@sheridancollege.ca>
2021-11-18 19:53:34 -0500

    added current day and month to top of id currentDay

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,7 +1,26 @@
 //version 1
 
 
-//display todays date as a heading
+//display todays date as a heading, in todaysDate Id
+
+var rightNow = new Date();
+
+console.log(rightNow);
+
+options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
+
+console.log(rightNow.toLocaleDateString("en-US", options));
+
+
+var rightNowText = JSON.stringify(rightNow.toLocaleDateString("en-US", options))
+
+console.log()
+
+
+var currentDayID = document.getElementById("currentDay");
+
+currentDayID.textContent = rightNowText.split('"')[1];
+
 
 
 //then display a hourly schedule with hour and task and save button as list
